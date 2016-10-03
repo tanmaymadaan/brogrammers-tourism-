69988f94f63efa960490eb444bcae98ef9ede74c
Aparnaakk <aparnaakk@gmail.com>
2016-10-03 15:34:00 +0530

    Add files via upload
    input for sad button on mood reccomender

---
+++ Sad.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Sad : MonoBehaviour {
+	public void SadString(){
+		string input1="";
+		input1=input1+"D";
+	}
+	private Button mybutton=null;
+	void Start() {
+		mybutton.onClick.AddListener (() => {
+			SadString ();
+		});
+		mybutton.interactable = false;
+	}
+
+
+}
